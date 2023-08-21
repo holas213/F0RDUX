3aeae4225355d450393b431f082cea2b0ae083e6
Luciano Kolln <luciano.kolln@unmsm.edu.pe>
2023-08-21 00:09:35 -0500

    Generacion del repositorio de los productos

---
+++ api/src/main/java/F0RDUX/api/repositories/ProductoRepository.java
@@ -0,0 +1,9 @@
+package F0RDUX.api.repositories;
+
+import F0RDUX.api.models.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto,Long> {
+}
