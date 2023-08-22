e91d36f4b5b25909997c24360eb5c0a12f556e5c
Luciano Kolln <luciano.kolln@unmsm.edu.pe>
2023-08-21 21:14:14 -0500

    Creando modelo de la entidad Pedido

---
+++ api/src/main/java/F0RDUX/api/models/Pedido.java
@@ -0,0 +1,29 @@
+package F0RDUX.api.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+public class Pedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
+    Long Id;
+    @Getter
+    @Setter
+    List<Producto> ListaProductos;
+    @Getter
+    @Setter
+    Long IdUsuario;
+    @Getter
+    @Setter
+    @Enumerated(value = EnumType.STRING)
+    EstadoPedido Estado;
+    @Getter
+    @Setter
+    Double Total;
+}
